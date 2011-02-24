369fe119cdb7fd08a9caf091a0a00ed2d2507b21
Daniel Bachhuber <danielbachhuber@gmail.com>
2011-02-24 21:21:30 +0000

    Count the number of responses

--- loadhomeworkassignments.js
+++ loadhomeworkassignments.js
@@ -3,8 +3,8 @@
  * Load the headlines from an array of website names
  */
 function db_load_site_headlines(websites, div, tag) {
-	jQuery.each(websites,
-	function(key, website) {
+	var response_counter = 0;
+	jQuery.each(websites, function(key, website) {
 		if (tag) {
 			var request_url = website + 'api/get_tag_posts/?count=1&slug='+tag;
 		} else {
@@ -15,14 +15,22 @@ function db_load_site_headlines(websites, div, tag) {
 			dataType: 'jsonp',
 			success: function(data) {
 				var html = '';
-				html += '<h4>';
-				if ( data.posts[0].author.first_name && data.posts[0].author.last_name ) {
-					html += data.posts[0].author.first_name + ': ';
-				} else if ()
-				html += '<a href="' + data.posts[0].url + '">' + data.posts[0].title + '</a></h4>';
-				html += '<p>' + data.posts[0].excerpt + '</p>';
-				jQuery(div).append(html);
+				if ( data.status == 'ok' ) {
+					html += '<h4>';
+					if ( data.posts[0].author.first_name && data.posts[0].author.last_name ) {
+						html += data.posts[0].author.first_name + ' ' + data.posts[0].author.last_name + ': ';
+					} else if ( data.posts[0].author.first_name ) {
+						html += data.posts[0].author.first_name + ': ';
+					}
+					html += '<a href="' + data.posts[0].url + '">' + data.posts[0].title + '</a></h4>';
+					html += '<p>' + data.posts[0].excerpt + '</p>';
+					jQuery(div).append(html);
+					response_counter++;
+					jQuery('h3 span.assignment-responses').html( response_counter );
+				}
 			},
 		});
 	});
+	var response_html = '<h3><span class="assignment-responses">' + response_counter + '</span> responses</h3>';
+	jQuery( div ).prepend( response_html );
 } // END db_load_site_headlines()
